2557242d934ff76bcb8879d96d1eef2989e8e657
Saqib Ali <saqibali.fd@gmail.com>
2025-02-26 22:59:15 +0500

    data get in two list

--- lib/controller/recipies_controller.dart
+++ lib/controller/recipies_controller.dart
@@ -5,23 +5,34 @@ import 'package:recipeapp/models/recipies_model.dart';
 class RecipiesController extends GetxController {
   RxBool isLoading = false.obs;
   final _firestore = FirebaseFirestore.instance;
-  RxList<RecipeModel> recipiesDataList = <RecipeModel>[].obs;
+  RxList<RecipeModel> fastFoodDataList = <RecipeModel>[].obs;
+  RxList<RecipeModel> drinkDataList = <RecipeModel>[].obs;
 
-  Future fetchRecipies() async {
+  Future<void> fetchRecipies() async {
     isLoading.value = true;
 
-    QuerySnapshot snapshot = await _firestore.collection('recipes').get();
-
-    List<RecipeModel> fetchedRecipies = snapshot.docs.map((doc) {
-      return RecipeModel.fromFirestore(doc.data() as Map<String, dynamic>);
-    }).toList();
-
-    print('************************${fetchedRecipies.length.toString()}');
-    recipiesDataList.assignAll(fetchedRecipies);
-
     try {
-      isLoading.value = false;
+      QuerySnapshot fastFoodSnapshot = await _firestore
+          .collection('recipes')
+          .where('category', isEqualTo: 'Fast Food')
+          .get();
+      QuerySnapshot drinkSnapshot = await _firestore
+          .collection('recipes')
+          .where('category', isEqualTo: 'Drink')
+          .get();
+
+      List<RecipeModel> fetchedFastFood = fastFoodSnapshot.docs.map((doc) {
+        return RecipeModel.fromFirestore(doc.data() as Map<String, dynamic>);
+      }).toList();
+      List<RecipeModel> fetchedDrink = drinkSnapshot.docs.map((doc) {
+        return RecipeModel.fromFirestore(doc.data() as Map<String, dynamic>);
+      }).toList();
+
+      fastFoodDataList.assignAll(fetchedFastFood);
+      drinkDataList.assignAll(fetchedDrink);
     } catch (e) {
+      print("Error fetching recipes: $e");
+    } finally {
       isLoading.value = false;
     }
   }
--- lib/view/user/home_screen.dart
+++ lib/view/user/home_screen.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:recipeapp/constants/app_icons.dart';
-import 'package:recipeapp/constants/app_images.dart';
 import 'package:recipeapp/controller/recipies_controller.dart';
 import 'package:recipeapp/view/user/detail_screen.dart';
 import 'package:recipeapp/widgets/card/product_card.dart';
@@ -15,65 +13,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-  final List foodList = [
-    {
-      'image': AppImages.dummyImage1,
-      'title': 'Cheez burger',
-      'category': 'Fast Food',
-      'time': '30',
-      'description':
-          'This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your mealThis food is very delecius you can use it in any dish you want in your meal'
-    },
-    {
-      'image': AppImages.dummyImage2,
-      'title': 'Pizza',
-      'category': 'Fast Food',
-      'time': '30',
-      'description':
-          'This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your mealThis food is very delecius you can use it in any dish you want in your meal'
-    },
-    {
-      'image': AppImages.dummyImage3,
-      'title': 'Sandvich',
-      'category': 'Fast Food',
-      'time': '30',
-      'description':
-          'This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your mealThis food is very delecius you can use it in any dish you want in your meal'
-    },
-    {
-      'image': AppImages.dummyImage4,
-      'title': 'Smosa',
-      'category': 'Fast Food',
-      'time': '30',
-      'description':
-          'This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your mealThis food is very delecius you can use it in any dish you want in your meal'
-    },
-    {
-      'image': AppImages.dummyImage5,
-      'title': 'Apple juice',
-      'category': 'Drink',
-      'time': '30',
-      'description':
-          'This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your mealThis food is very delecius you can use it in any dish you want in your meal'
-    },
-    {
-      'image': AppImages.dummyImage6,
-      'title': 'Banana juice',
-      'category': 'Drink',
-      'time': '30',
-      'description':
-          'This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your mealThis food is very delecius you can use it in any dish you want in your meal'
-    },
-    {
-      'image': AppImages.dummyImage7,
-      'title': 'Lemon juice',
-      'category': 'Drink',
-      'time': '30',
-      'description':
-          'This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your meal This food is very delecius you can use it in any dish you want in your mealThis food is very delecius you can use it in any dish you want in your meal'
-    },
-  ];
-
   final RecipiesController recipiesController = Get.put(RecipiesController());
 
   late TabController tabController;
@@ -81,21 +20,28 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     tabController = TabController(length: 2, vsync: this);
+    recipiesController.fetchRecipies();
   }
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
 
-    List fastFoodList =
-        foodList.where((item) => item['category'] == 'Fast Food').toList();
-    List drinkList =
-        foodList.where((item) => item['category'] == 'Drink').toList();
+    // List fastFoodList =
+    //     foodList.where((item) => item['category'] == 'Fast Food').toList();
+    // List drinkList =
+    //     foodList.where((item) => item['category'] == 'Drink').toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
-        actions: [IconButton(onPressed: () {}, icon: Icon(AppIcons.theme))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                recipiesController.fetchRecipies();
+              },
+              icon: Icon(AppIcons.theme))
+        ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -122,125 +68,64 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               key: ValueKey<int>(tabController.index),
               controller: tabController,
               children: [
-                StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('recipes')
-                      .where('category', isEqualTo: 'Fast Food')
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    }
-                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                      return const Center(child: Text('No recipes found.'));
-                    }
-                    return GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: .8,
-                      ),
-                      itemCount: snapshot.data!.docs.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        final doc = snapshot.data!.docs[index];
-                        return GestureDetector(
-                          onTap: () => Get.to(() => DetailScreen()),
-                          child: ProductCard(
-                            productImage: doc['image'],
-                            title: doc['title'],
-                            category: doc['category'],
-                            time: doc['time'],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-                // Obx(
-                //   () {
-                //     if (recipiesController.isLoading == true) {
-                //       return CircularProgressIndicator();
-                //     } else if (recipiesController.recipiesDataList.isEmpty) {
-                //       Text('Data not found');
-                //     }
-
-                //     return GridView.builder(
-                //       gridDelegate:
-                //           const SliverGridDelegateWithFixedCrossAxisCount(
-                //         crossAxisCount: 2,
-                //         childAspectRatio: 1.1,
-                //       ),
-                //       itemCount: recipiesController.recipiesDataList.length,
-                //       shrinkWrap: true,
-                //       itemBuilder: (context, index) {
-                //         return GestureDetector(
-                //           onTap: () {
-                //             recipiesController.fetchRecipies();
-                //           },
-                //           // onTap: () => Get.to(() => DetailScreen()),
-                //           child: ProductCard(
-                //             productImage: recipiesController
-                //                 .recipiesDataList[index].image,
-                //             title: recipiesController
-                //                 .recipiesDataList[index].title,
-                //             category: recipiesController
-                //                 .recipiesDataList[index].category,
-                //             time: recipiesController
-                //                 .recipiesDataList[index].time,
-                //           ),
-                //           // child: ProductCard(
-                //           //   productImage: fastFoodList[index]['image'],
-                //           //   title: fastFoodList[index]['title'],
-                //           //   category: fastFoodList[index]['category'],
-                //           //   time: fastFoodList[index]['time'],
-                //           // ),
-                //         );
-                //       },
-                //     );
-                //   },
-                // ),
-                StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('recipes')
-                      .where('category', isEqualTo: 'Drink')
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    }
-                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                      return const Center(child: Text('No recipes found.'));
-                    }
-                    return GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: .8,
-                      ),
-                      itemCount: snapshot.data!.docs.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        final doc = snapshot.data!.docs[index];
-                        return GestureDetector(
-                          onTap: () => Get.to(() => DetailScreen()),
-                          child: ProductCard(
-                            productImage: doc['image'],
-                            title: doc['title'],
-                            category: doc['category'],
-                            time: doc['time'],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
+                Obx(() {
+                  if (recipiesController.isLoading.value) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (recipiesController.fastFoodDataList.isEmpty) {
+                    return const Center(child: Text('No recipes found.'));
+                  }
+                  return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: .8,
+                    ),
+                    itemCount: recipiesController.fastFoodDataList.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      final recipe = recipiesController.fastFoodDataList[index];
+                      return GestureDetector(
+                        onTap: () => Get.to(() => DetailScreen()),
+                        child: ProductCard(
+                          productImage: recipe.image,
+                          title: recipe.title,
+                          category: recipe.category,
+                          time: recipe.time,
+                        ),
+                      );
+                    },
+                  );
+                }),
+                Obx(() {
+                  if (recipiesController.isLoading.value) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (recipiesController.fastFoodDataList.isEmpty) {
+                    return const Center(child: Text('No recipes found.'));
+                  }
+                  return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: .8,
+                    ),
+                    itemCount: recipiesController.drinkDataList.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      final recipe = recipiesController.drinkDataList[index];
+                      return GestureDetector(
+                        onTap: () => Get.to(() => DetailScreen()),
+                        child: ProductCard(
+                          productImage: recipe.image,
+                          title: recipe.title,
+                          category: recipe.category,
+                          time: recipe.time,
+                        ),
+                      );
+                    },
+                  );
+                }),
               ],
             ),
           )
